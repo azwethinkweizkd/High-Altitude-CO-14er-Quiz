0c67e0a38729ebcbb0d0a66120eae12dfbfc2347
Kevin Devlin <azwethinkweizkd@gmail.com>
2021-04-25 13:45:22 -0600

    Added elements and h1 to index.html, added event listener to start quiz and timer

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+//Selects the element and creates the timer where the time left in the quiz will be displayed
+var timeElement = document.querySelector(".timer");
+
+var mainEl = document.getElementById("main");
+
+var takeQuizEl = document.querySelector("#take-quiz");
+//Creates variable for how long the quiz will last
+var timeRemaining = 60;
+
+//Funtion that will create the timer for how long you create it for
+function settime() {
+  var timeInterval = setInterval(function () {
+    timeRemaining--;
+    timeElement.textContent = "Quiz timer: " + timeRemaining;
+
+    if (timeRemaining === 0) {
+      clearInterval(timeInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+
+takeQuizEl.addEventListener("click", function () {
+  settime();
+});
+
+function sendMessage() {
+  mainEl.textContent = "";
+  var endMsg = mainEl.createElement("div");
+  endMsg.textContent = "Quiz has ended";
+  mainEl.appendChild(endMsg);
+}
